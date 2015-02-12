366094328b73c8739832aae16c22af3914f7ce9c
Andrew Hautau <arhautau@gmail.com>
2015-02-11 21:07:24 -0800

    Initial Commit

---
+++ ARHautauTodo/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  ARHautauTodo
+//
+//  Created by Andrew Hautau on 2/11/15.
+//  Copyright (c) 2015 Andrew Hautau. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
